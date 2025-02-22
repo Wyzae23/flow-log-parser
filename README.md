38909a31c18495db7e235396c5fa542ed4840df8
Harold <haroldmo23@gmail.com>
2025-02-22 13:31:55 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Flow Log Parser
+
+## Overview
+
+The script performs the following tasks:
+- Reads a plain text file containing log flow records
+- Reads a CSV lookup table mapping `(dsport, protocol)` combinations to tags
+- Processes each flow log entry and determines the destination port and protocol, matching it to a tag
+- Outputs two CSV files:
+    - One with counts for each tag
+    - One with counts for each `(port, protocol)` combination
+
+## Assumptions
+- The input file is a plain text file that is a (`.txt`) file
+- The lookup table is given as a (`.csv`) file
+- The output files are outputted as (`.csv`) files
+- Any row in the input flow logs file that doesn't contain all 14 fields are skipped and viewed as invalid rows
+- Only rows with protocol numbers from 0-19 inclusive are considered valid rows
+- The `.csv` file that holds the tag counts will have a tag called `Untagged`. This tag is for any valid row that did not contain a (port, protocol) mapping in the lookup table
+- the `.csv` file that holds the (port, protocol) combination counts will only hold a count for valid rows.
+
+
+## Requirements
+- Python 3.x
+- No external libraries are needed, only the Python standard library
+
+## How to Run
+- Terminal Command: `python script.py /path/to/input.txt /path/to/lookupTable.csv`
+- Example with input file and lookup table file in the same directory:
+    `python script.py exampleInput.txt exampleLookupTable.csv`
+
+
+## Input Files
+- *Flow Log File:*
+    A plain text file (`.txt`) where each line represents a flow log record with space-separated fields (expected 14 fields per line)
+
+- *Lookup Table:*
+    A CSV file (`.csv`) with a header and three columns:
+    `dstport, protocol, tag`
+    Each row maps a destination port and protocol combination to a tag
+
+## Output Files
+The script generates two CSV output files:
+- `tag_counts_for<lookup_table_filename>_and_<input_file_filename>.csv`: Contains tag counts with columns `Tag, Count`
+- `port_protocol_counts_for<lookup_table_filename>_and_<input_file_filename>.csv`: Contains counts for port, protocol combination with columns `Port, Protocol, Count`
+
+
---
+++ script.py
@@ -0,0 +1,125 @@
+import csv
+import argparse
+
+# protocol mapping from 
+# https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml
+protocolMap = {
+    "0": "hopopt",
+    "1": "icmp",
+    "2": "igmp",
+    "3": "ggp",
+    "4": "ip",
+    "5": "st",
+    "6": "tcp",
+    "7": "cbt",
+    "8": "egp",
+    "9": "igp",
+    "10": "bbn-rcc-mon",
+    "11": "nvp-ii",
+    "12": "pup",
+    "13": "argus",
+    "14": "emcon",
+    "15": "xnet",
+    "16": "chaos",
+    "17": "udp",
+    "18": "mux",
+    "19": "dcn-meas"
+}
+
+# removes extension from filename
+def removeExtension(filename):
+    parts = filename.split(".")
+    if len(parts) == 1:
+        return filename
+    return '.'.join(parts[:-1])
+
+# generates lookup table from csv input
+def loadLookupTable(filepath):
+    lookup = {}
+    with open(filepath, "r", newline="") as f:
+        reader = csv.reader(f)
+        next(reader)
+        for row in reader:
+            key = (row[0], row[1])
+            lookup[key] = row[2]
+    return lookup
+
+# process each row of the flow log
+def processFlowLogs(filepath, lookup):
+    tagCounts = {}
+    keyCounts = {}
+    totalInputs = 0
+
+    with open(filepath, "r", newline="") as f:
+        for line in f:
+            fields = line.split()
+
+            # if there isn't exactly 14 fields in the row, it's invalid
+            if not fields or len(fields) != 14:
+                continue
+            dstPort = fields[6]
+            protocol = None
+
+            # check if the protocol is in our protocolMap
+            # if protocol isn't in mapping, then we can skip this row
+            # as it's invalid
+            if fields[7] in protocolMap:
+                protocol = protocolMap[fields[7]]
+
+            # for valid rows, process them
+            if dstPort and protocol:
+                totalInputs += 1
+                key = (dstPort, protocol)
+                keyCounts[key] = keyCounts.get(key, 0) + 1
+                if key in lookup:
+                    tag = lookup[key]
+                    tagCounts[tag] = tagCounts.get(tag, 0) + 1
+                    
+        # generate "Untagged" count for the tagCounts map
+        totalTagged = sum(tagCounts.values())
+        tagCounts["Untagged"] = totalInputs - totalTagged
+
+        return tagCounts, keyCounts
+    
+# generate a CSV with `filename`, with headers as `header`, and with row `rows`
+def writeCSV(filename, header, rows):
+    with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+        for row in rows:
+            writer.writerow(row)
+
+
+def main ():
+    # set up parser/arguments
+    parser = argparse.ArgumentParser(description="Parsing input log file and lookup table")
+    parser.add_argument("input_file")
+    parser.add_argument("lookup_table")
+    args = parser.parse_args()
+
+    # grab filepaths for input file and lookup table
+    lookupTableFilepath = args.lookup_table
+    inputFilepath = args.input_file
+
+    lookup = loadLookupTable(lookupTableFilepath)
+
+    tagCounts, keyCounts = processFlowLogs(inputFilepath, lookup)
+    
+    # generate rows for tag counts output file
+    tagRows = []
+    for tag in tagCounts:
+        tagRows.append([tag, tagCounts[tag]])
+    tagCountsFilename = f"tag_counts_for_{removeExtension(lookupTableFilepath)}_and_{removeExtension(inputFilepath)}.csv"
+    writeCSV(tagCountsFilename, ["Tag", "Count"], tagRows)
+    print(f"Generated output CSV for {tagCountsFilename}")
+
+    # generate rows for port protocol combination counts output file
+    portProtocolRows = []
+    for (dstPort, protocol) in keyCounts:
+        portProtocolRows.append([dstPort, protocol, keyCounts[(dstPort, protocol)]])
+    portProtocolCountsFilename = f"port_protocol_counts_for_{removeExtension(lookupTableFilepath)}_and_{removeExtension(inputFilepath)}.csv"
+    writeCSV(portProtocolCountsFilename, ["Port", "Protocol", "Count"], portProtocolRows)
+    print(f"Generated output CSV for {portProtocolCountsFilename}")
+
+if __name__ == "__main__":
+    main()
